8986eed87291f84ff1228ea44398658562f8026b
joan <joanbenaventcastells@gmail.com>
2023-02-08 13:55:06 +0100

    Primer a versio de mostrar categories

--- IscaBar/Views/CategoryListView.xaml.cs
+++ IscaBar/Views/CategoryListView.xaml.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using IscaBar.Models;
+using IscaBar.ViewModels;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -12,9 +14,28 @@ namespace IscaBar.Views
     [XamlCompilation(XamlCompilationOptions.Compile)]
     public partial class CategoryListView : ContentPage
     {
+        CategoryListVM vm = new CategoryListVM();
+        
         public CategoryListView()
         {
             InitializeComponent();
+            BindingContext = vm;
+        }
+     
+        protected override async void OnAppearing()
+        {
+            base.OnAppearing();
+            await vm.LoadCategoriesAsync();
+        }
+    
+        async void Handle_ItemTapped(object sender, ItemTappedEventArgs e)
+        {
+            Category category = (Category)e.Item;
+            if (category == null)
+                return;
+
+
+            ((ListView)sender).SelectedItem = null;
         }
     }
 }
